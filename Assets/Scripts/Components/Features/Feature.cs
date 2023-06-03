dfe0d547228a5200b3b41a43ad67de98ff84b8b2
Cristian Cordoba <cristiandc10@live.com>
2023-06-03 00:29:50 -0400

    Forgot to update Feature class

--- Assets/Scripts/Components/Features/Feature.cs
+++ Assets/Scripts/Components/Features/Feature.cs
@@ -6,4 +6,9 @@ public class Feature : MonoBehaviour, IComponent
 {
     public string Name { get; set; }
     public string Description { get; set; }
+    public string ShortDescription { get; set; }
+    public List<IComponentBase> Components { get; set; }
+    public bool Locked { get; set; }
+    public int UnlockValue { get; set; }
+    public string LockStat { get; set; }
 }
